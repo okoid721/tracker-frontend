663e5a813f65826dc34fecfa9c429ad5812e78e3
okoid721 <okoid721@gmail.com>
2024-03-25 23:43:34 +0100

    eccomerce

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Orb from './component/Orb/Orb';
+import Navigation from './component/navigation/Navigation';
 
 function App() {
   return (
     <div className=" h-full relative ">
       <Orb />
-      <main className=" p-8 h-full flex gap-8"></main>
+      <main className=" p-8 h-full flex gap-8">
+        <Navigation />
+      </main>
     </div>
   );
 }
--- src/component/navigation/Navigation.jsx
+++ src/component/navigation/Navigation.jsx
@@ -4,9 +4,13 @@ import { MdRemove } from 'react-icons/md';
 
 const Navigation = () => {
   return (
-    <div>
-      <div>
-        <img src="https://avatar.iran.liara.run/public" alt="" />
+    <div className=" px-6 py-8 w-[300px] h-[100%] bg-[#d7d7d7] border-[3px] border-white rounded-[32px] ">
+      <div className=" h-[100px] flex items-center gap-4">
+        <img
+          src="https://avatar.iran.liara.run/public"
+          alt=""
+          className=" w-[80px] h-[80px] rounded-[50%]"
+        />
         <div>
           <h2>Mike</h2>
           <p>Your Money</p>
@@ -15,7 +19,7 @@ const Navigation = () => {
       <ul>
         {menuItems.map((item) => {
           return (
-            <li key={item.id}>
+            <li key={item.id} className="  flex-col">
               {item.icon}
               <span>{item.title}</span>
             </li>
