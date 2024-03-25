3ce98ca591e7f815df7cfbda7086a2e1c7c00253
okoid721 <okoid721@gmail.com>
2024-03-26 00:10:17 +0100

    tracker

--- src/component/navigation/Navigation.jsx
+++ src/component/navigation/Navigation.jsx
@@ -1,33 +1,37 @@
 import React from 'react';
 import { menuItems } from '../../utils/menuItems';
-import { MdRemove } from 'react-icons/md';
+import { TbLogout } from 'react-icons/tb';
 
 const Navigation = () => {
   return (
-    <div className=" px-6 py-8 w-[300px] h-[100%] bg-[#d7d7d7] border-[3px] border-white rounded-[32px] ">
+    <div className=" px-6 py-8 lg:w-screen w-[100px] h-[100%] lg:h-[70px] bg-[#d7d7d7] border-[3px] border-white rounded-[32px] flex items-center justify-between ">
       <div className=" h-[100px] flex items-center gap-4">
         <img
           src="https://avatar.iran.liara.run/public"
           alt=""
-          className=" w-[80px] h-[80px] rounded-[50%]"
+          className=" w-[50px] h-[50px] rounded-[50%]"
+          s
         />
-        <div>
-          <h2>Mike</h2>
-          <p>Your Money</p>
+        <div className=" flex items-center gap-3">
+          <h2 className=" font-bold text-3xl">Mike</h2>
+          <p className=" font-semibold">Your Money</p>
         </div>
       </div>
-      <ul>
+      <ul className=" flex lg:flex-row gap-5 items-center ">
         {menuItems.map((item) => {
           return (
-            <li key={item.id} className="  flex-col">
+            <li
+              key={item.id}
+              className="  flex-col  items-center text-center flex justify-center"
+            >
               {item.icon}
-              <span>{item.title}</span>
+              <span className=" hidden lg:flex">{item.title}</span>
             </li>
           );
         })}
       </ul>
       <div>
-        <li>{<MdRemove />} SignOut </li>
+        <li className="flex items-center flex-col ">{<TbLogout />} SignOut </li>
       </div>
     </div>
   );
