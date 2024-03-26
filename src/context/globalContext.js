542533fc6e244eff8357460a8afe0211f3b41c07
okoid721 <okoid721@gmail.com>
2024-03-26 23:43:11 +0100

    eccomerce

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5000/api/v1/';
@@ -6,5 +6,24 @@ const BASE_URL = 'http://localhost:5000/api/v1/';
 const GlobalContext = React.createContext();
 
 export const GlobalProvider = ({ children }) => {
-  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+  const [incomes, setIncomes] = useState([]);
+  const [expenses, setExpenses] = useState([]);
+  const [saving, setSaving] = useState([]);
+  const [error, setError] = useState(null);
+
+  const addIncome = async () => {
+    try {
+      const response = await axios.post(`${BASE_URL}add-income`, incomes);
+    } catch (error) {
+      setIncomes(error.response.data.message);
+      console.log('error in globalContext addincome');
+    }
+  };
+  return (
+    <GlobalContext.Provider value={'hello'}>{children}</GlobalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return React.useContext(GlobalContext);
 };
