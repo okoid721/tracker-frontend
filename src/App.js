c68066e6c11d7c8ca2fdf40244e49e1db8d22671
okoid721 <okoid721@gmail.com>
2024-03-26 18:09:24 +0100

    eccomerce

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import Orb from './component/Orb/Orb';
 import Navigation from './component/navigation/Navigation';
 
 function App() {
+  const [active, setActive] = useState(1);
+
   return (
     <div className=" h-full relative ">
       <Orb />
       <main className=" lg:p-8 p-2 h-full flex gap-8">
-        <Navigation />
+        <Navigation active={active} setActive={setActive} />
       </main>
     </div>
   );
