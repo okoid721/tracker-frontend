9bfdd07bb8916f91440c402a6330981ad3c29077
okoid721 <okoid721@gmail.com>
2024-03-29 00:19:50 +0100

    tracker

--- src/component/from/Form.jsx
+++ src/component/from/Form.jsx
@@ -3,7 +3,6 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useGlobalContext } from '../../context/globalContext';
 import Button from '../Button/Button';
-import toast from 'react-hot-toast';
 
 const Form = () => {
   const { addIncome, getIncome, error } = useGlobalContext();
@@ -44,7 +43,6 @@ const Form = () => {
       className="text-black flex flex-col lg:gap-[2rem] gap-[10px]"
       onSubmit={handleSubmit}
     >
-      {error && toast.error}
       <div className="w-[100%]">
         <input
           type="text"
